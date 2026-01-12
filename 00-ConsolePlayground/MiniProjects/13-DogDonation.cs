710563bad69ea51d8caca2eeacbf937bee9f83f7
andy <32784981+lgaoandy@users.noreply.github.com>
2026-01-12 15:16:58 -0700

    added my solution to the challenge project on Mini #13 and added pastel package

--- 00-ConsolePlayground/MiniProjects/13-DogDonation.cs
+++ 00-ConsolePlayground/MiniProjects/13-DogDonation.cs
@@ -1,25 +1,30 @@
 // Following code is my work for the guided project offered in microsoft (omitting their guidance)
-// Reference material: https://learn.microsoft.com/en-us/training/modules/guided-project-work-variable-data-c-sharp/2-prepare
+using System.Drawing;
+using Pastel;
 
 namespace MiniProjects.DogDonation
 {
     class DogDonation
     {
-        static string AskUser(string prompt)
+        static string AskUser(string prompt = "")
         {
             string? userResponse;
             while (true)
             {
-                Console.WriteLine(prompt);
+                Console.Write(prompt);
                 userResponse = Console.ReadLine();
                 if (userResponse == null || userResponse.Trim().Length == 0)
                     Console.Write("You must enter something...");
                 else
-                    return userResponse;
+                {
+                    Console.WriteLine();
+                    return userResponse.Trim().ToLower();
+                }
             }
         }
 
-        public static void Run()
+        // Reference material: https://learn.microsoft.com/en-us/training/modules/guided-project-work-variable-data-c-sharp/2-prepare
+        public static void RunGuided()
         {
             // #1 the ourAnimals array will store the following: 
             string animalSpecies = "";
@@ -151,7 +156,6 @@ namespace MiniProjects.DogDonation
                     case "2":
                         // Gather user input for the pet characteristic search
                         readResult = AskUser("Please enter what characteristics you're looking for: ");
-                        readResult = readResult.Trim().ToLower();
 
                         // Loop through animals and identify dogs
                         string[] matchingIds = new string[8];
@@ -199,6 +203,146 @@ namespace MiniProjects.DogDonation
 
             } while (menuSelection != "exit");
 
+        }
+
+        // Add overloaded method to force user to choice an acceptable answer
+        static string AskUser(string[] answers, string prompt = "")
+        {
+            string? userResponse;
+            while (true)
+            {
+                Console.Write(prompt);
+                userResponse = Console.ReadLine();
+
+                if (userResponse == null || userResponse.Trim().Length == 0)
+                {
+                    Console.Write("You must enter something... ");
+                    continue;
+                }
+
+                userResponse = userResponse.Trim().ToLower();
+                if (answers.Contains(userResponse))
+                    return userResponse;
+                else
+                    Console.Write("Your response is not an available option. Try again... ");
+            }
+        }
+
+        static void PressAnyKeyToContinue()
+        {
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+            Console.WriteLine();
+        }
+
+        static void DisplayMainMenu()
+        {
+            Console.WriteLine("\n" + " MAIN MENU OPTIONS ".PadLeft(24, '-').PadRight(36, '-'));
+            Console.WriteLine("1.".PadLeft(4).PadRight(6) + "List all our current pet information");
+            Console.WriteLine("2.".PadLeft(4).PadRight(6) + "Search dogs by characteristic");
+            Console.WriteLine("3.".PadLeft(4).PadRight(6) + "Enter (3 or exit) to leave program");
+        }
+
+        static void DisplayDog(string[,] dogs, int i)
+        {
+            Console.WriteLine($"{dogs[i, 1]} (ID# {dogs[i, 0]})");
+            Console.WriteLine($"{dogs[i, 2]} years old");
+            Console.WriteLine($"Physical Description: {dogs[i, 3]}");
+            Console.WriteLine($"Personality: {dogs[i, 4]}");
+            Console.WriteLine($"Fee: {dogs[i, 5]}\n");
+        }
+
+        static void DisplayDogs(string[,] dogs)
+        {
+            Console.WriteLine();
+            for (int i = 0; i < dogs.GetLength(0); i++)
+                DisplayDog(dogs, i);
+            PressAnyKeyToContinue();
+        }
+
+        static void SearchCharacteristics(string[,] dogs)
+        {
+            // Prompt user for search in comma separated list
+            string userResponse = AskUser("\nIn a comma-separated list, Enter the traits you are seeking: ");
+            string[] traits = userResponse.Split(',');
+            int n = traits.Length;
+
+            // Trim and lowercase all traits
+            for (int i = 0; i < n; i++)
+                traits[i] = traits[i].Trim().ToLower();
+
+            int count = 0;
+            for (int i = 0; i < dogs.GetLength(0); i++)
+            {
+                bool match = false;
+                for (int j = 0; j < n; j++) // Checks if any traits match current dog
+                {
+                    string dogDescription = dogs[i, 3].Trim().ToLower() + " " + dogs[i, 4].Trim().ToLower();
+                    if (dogDescription.Contains(traits[j]))
+                    {
+                        Console.WriteLine($"Our dog {dogs[i, 1]} is a {traits[j].Pastel(Color.Gold)} match!");
+                        match = true;
+                    }
+                }
+
+                if (match) // If match, displays their traits
+                {
+                    count++;
+                    Console.WriteLine();
+                    DisplayDog(dogs, i);
+                }
+            }
+
+            if (count == 0)
+                Console.WriteLine("There are no dogs with your characteristics!");
+            PressAnyKeyToContinue();
+        }
+
+        // Reference material: https://learn.microsoft.com/en-us/training/modules/challenge-project-work-variable-data-c-sharp/2-prepare
+        public static void RunChallenge()
+        {
+            // Rewritting starter code - start with initializing animals at declaration
+            string[,] ourDogs = new string[,]
+            {
+                {
+                    "d1",
+                    "Lola",
+                    "2",
+                    "Medium sized cream colored female golden retriever weighing about 45 pounds. housebroken.",
+                    "Loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.",
+                    "$85.00"
+                },
+                {
+                    "d2",
+                    "Gus",
+                    "9",
+                    "Large reddish-brown male golden retriever weighing about 85 pounds. housebroken.",
+                    "Loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.",
+                    "$49.99"
+                },
+            };
+
+            string menuSelection;
+            Console.WriteLine("Welcome to the Contoso PetFriends app!");
+
+            while (true)
+            {
+                DisplayMainMenu();
+                menuSelection = AskUser(["1", "2", "3", "exit"], "Enter your selection: ");
+
+                switch (menuSelection)
+                {
+                    case "1":
+                        DisplayDogs(ourDogs);
+                        break;
+                    case "2":
+                        SearchCharacteristics(ourDogs);
+                        break;
+                    case "3":
+                    case "exit":
+                        return;
+                }
+            }
         }
     }
 }
--- 00-ConsolePlayground/Program.cs
+++ 00-ConsolePlayground/Program.cs
@@ -32,6 +32,7 @@ class Program
         // ArrayManipulation.SortId("B123,C234,A345,C15,B177,G3003,C235,B179");
         // StringInterpolation.PrintShares();
         // StringExtraction.ExtractHTMLExercise();
-        DogDonation.Run();
+        // DogDonation.RunGuided();
+        DogDonation.RunChallenge();
     }
 }
