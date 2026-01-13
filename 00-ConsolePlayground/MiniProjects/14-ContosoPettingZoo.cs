d0a2cfce25b8a440ffd9db82605c765ffcb1b73f
andy <32784981+lgaoandy@users.noreply.github.com>
2026-01-12 18:18:55 -0700

    added miniproject #14 - guided projects for methods under microsoft

---
+++ 00-ConsolePlayground/MiniProjects/14-ContosoPettingZoo.cs
@@ -0,0 +1,22 @@
+namespace MiniProjects.ContosoPettingZoo
+{
+    class ContosoPettingZoo
+    {
+        public string[] Animals = [
+            "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
+            "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
+            "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises"
+        ];
+
+        // Reference material: https://learn.microsoft.com/en-us/training/modules/guided-project-visit-petting-zoo/
+        public static void PlanSchoolVisit(string schoolName, int groups = 6)
+        {
+            // Randomize animals
+        }
+
+        public static void RandomizeAnimals()
+        {
+
+        }
+    }
+}
