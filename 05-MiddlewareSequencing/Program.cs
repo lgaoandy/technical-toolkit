7c94db72f404680c7d3b5ba61c03e728dabb7927
Andy Gao <andygaoofficial@gmail.com>
2026-01-26 10:29:55 -0700

    added request logger and path filter middleware on project #5

--- 05-MiddlewareSequencing/Program.cs
+++ 05-MiddlewareSequencing/Program.cs
@@ -1,6 +1,42 @@
+using System.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+/*  Request Logger 
+    - logs when a request starts and when it completes, logs time taken
+*/
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"Request started: {context.Request.Method} {context.Request.Path}");
+    
+    // Time request
+    var stopwatch = Stopwatch.StartNew();
+    await next();
+    stopwatch.Stop();
+    long milliseconds = stopwatch.ElapsedMilliseconds;    
+
+    Console.WriteLine($"Request completed in {milliseconds} ms");
+});
+
+/*  Path Filter 
+    - Only allows requests to paths starting with /api/
+*/
+app.Use(async (context, next) =>
+{
+    string path = context.Request.Path;
+    
+    if (!path.StartsWith("/api/"))
+    {
+        context.Response.StatusCode = 404;
+        context.Response.ContentType = "application/json";
+        await context.Response.WriteAsync("{\"error\": \"request path does not start with the required /api/\"}");
+        return;
+    }
+        
+    await next();
+});
 
 app.Run();
