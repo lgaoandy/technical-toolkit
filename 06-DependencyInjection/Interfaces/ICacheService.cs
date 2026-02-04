03ad04422aced39c03d6589f4c6ca63d076da113
andy <32784981+lgaoandy@users.noreply.github.com>
2026-02-04 12:49:04 -0700

    Implement ICacheService interface with cache functionality.

--- 06-DependencyInjection/Interfaces/ICacheService.cs
+++ 06-DependencyInjection/Interfaces/ICacheService.cs
@@ -2,5 +2,10 @@ namespace DependencyInjection.Interfaces;
 
 public interface ICacheService
 {
-
+    void Set<T>(string key, T? value);
+    T Get<T>(string key);
+    bool TryGet<T>(string key, out T? value);
+    void Remove(string key);
+    int GetCacheHits();
+    int GetCacheMisses();
 }
